275b82abf391c79b465fbf80d63292a3c0398595
tswinyane <alfred@F006472.FPD.local>
2025-08-19 06:40:48 +0200

    included the header file for styling

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <!--<title>Register</title>-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/[email protected]/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://www.google.com/recaptcha/api.js"></script>
+        
+        <script>
+            $(document).ready(function(){
+                $('[data-toggle="tooltip"]').tooltip();   
+            });
+        </script>
+        
+        <!-- Files for the modal popup -->
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
+
+
+        <style>
+            .wrapper{
+                width: 500px;
+                margin: 0 auto;
+            }
+            table tr td:last-child{
+                width: 120px;
+            }
+            .error {
+                color: #FF0000;
+            }
+            
+            body {
+                background-image: url(https://datagallery.fpdsiu.co.za/images/FPD%20Design%20Element%20-%20BlueGreen.png); 
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+                min-height: 100vh;
+            }
+
+            .container_login {
+                background: #ffffff;
+                padding: 10px 50px;
+                border-radius: 30px;
+                border: 2px solid #f6a220;
+                width: 50%;
+            }
+            .container {
+                background: #ffffff;
+                padding: 10px 50px;
+                border-radius: 30px;
+                border: 2px solid #f6a220;
+                width: 50%;
+            }
+
+            .btn {
+                width: 100%;
+                border-radius: 30px;
+                background-color: #1f3540;
+                color: #ffffff;
+                border: 0px;
+            }
+        </style>
+            
+            
+            
+        
+    </head>
+<body>
